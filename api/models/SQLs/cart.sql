4afa04c614b3befb56bd317a2745efcc7cdf8e93
Surojit Basu <surojit19@gmail.com>
2022-08-10 10:57:18 +0530

    Cart table

---
+++ api/models/SQLs/cart.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `cart` ( 
+  `id` INT(11) NOT NULL AUTO_INCREMENT , 
+  `user_id` INT(11) NOT NULL , 
+  `course_id` INT(11) NOT NULL , 
+  `course_resources` JSON NULL , 
+  `price` DECIMAL(10,2) NOT NULL , 
+  `is_bundle` BOOLEAN NOT NULL DEFAULT FALSE , 
+  `discount` DECIMAL(10,2) NULL , 
+  `coupon_code` VARCHAR(50) NULL , 
+  `status` ENUM('queue','processing','paid','cancelled') NOT NULL DEFAULT 'queue' , 
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+  `updated_at` DATETIME NOT NULL , 
+  PRIMARY KEY (`id`)) ENGINE = InnoDB;
