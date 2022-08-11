977e435de30f52ae77bf56f90d3cc7e202783087
rajesh349 <rajeshs@knowledgesynonyms.com>
2022-08-11 18:32:17 +0530

    api cart

--- api/models/CartModel.js
+++ api/models/CartModel.js
@@ -12,6 +12,18 @@ class CartModel extends BaseModel {
     return this.deleteWhere({user_id: data.user_id, course_id: data.course_id, status: 'queue'}).then(() => super.add(data));
   }
 
+  getCartData(data){
+    console.log(data);
+    SqlQuery = `SELECT IF(ca.user_id = ?, c.name, c.slug)slug, c.name, ca.* FROM cart ca
+    LEFT JOIN courses c ON ca.course_id = c.id  
+    WHERE ca.user_id = ?`
+    return this.list(SqlQuery)
+    .then(res =>{
+      console.log(res);
+    })
+  }
+
 }
+
 
 module.exports = CartModel;
