ce998a5f10a6e106b8ce6d46dace31348f5369ea
rajesh349 <rajeshs@knowledgesynonyms.com>
2022-09-02 12:03:58 +0530

    forget pass API

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -240,7 +240,8 @@ class UserModel extends BaseModel {
     let html = `<p>Hi ${name}),</p>
     <p>${process.env.APP_NAME}.</p>
     <p>We have processed your request to reset your password.</p>
-    <p><a href="${process.env.APP_URL}/resetpass?t=${token}">Click here</a> to reset your password</p>
+    <p><a href="${process.env.APP_URL}/resetpass/${token}">Click here</a> to reset your password</p>
+    <p>Click Here ${process.env.APP_URL}/resetpass/${token}</p>
     <p>Please do not share your credentials to avoid sensitive data breach.</p>
     Good Luck.<br />
     Administrator`;
