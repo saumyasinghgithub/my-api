81e06c727283be352bb0f32d276ba4692d412c64
rajesh349 <rajeshs@knowledgesynonyms.com>
2022-09-23 11:00:17 +0530

    Social DbTable

---
+++ api/models/SQLs/sociallinks.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 23, 2022 at 05:26 AM
+-- Server version: 10.1.45-MariaDB
+-- PHP Version: 7.2.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ks_ad_crmdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sociallinks`
+--
+
+CREATE TABLE IF NOT EXISTS `sociallinks` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `class` text NOT NULL,
+  `link` text NOT NULL,
+  `active` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sociallinks`
+--
+
+INSERT INTO `sociallinks` (`id`, `title`, `class`, `link`, `active`, `created_at`, `updated_at`) VALUES
+(3, 'facebook', 'fab fa-facebook text-light', 'https://www.facebook.com/', 1, '2022-09-09 07:29:57', '0000-00-00 00:00:00'),
+(4, 'linkedIn', 'fab fa-linkedin', 'https://www.linkedin.com/', 1, '2022-09-09 07:30:10', '0000-00-00 00:00:00'),
+(5, 'instagram', 'fab fa-instagram', 'https://instagram.com', 1, '2022-09-09 07:30:34', '0000-00-00 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sociallinks`
+--
+ALTER TABLE `sociallinks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sociallinks`
+--
+ALTER TABLE `sociallinks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
