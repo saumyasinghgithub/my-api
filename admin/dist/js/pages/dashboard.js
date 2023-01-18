f56a5e34e5727674557213ff78a364bf6c136cb5
rajesh349 <rajeshs@knowledgesynonyms.com>
2023-01-18 18:31:44 +0530

    Admin Build Dashboard chnages

--- admin/dist/js/pages/dashboard.js
+++ admin/dist/js/pages/dashboard.js
@@ -111,13 +111,15 @@ $(function () {
     height: '250px'
   })
 
+window['orderChart'] = (data) => {
   /* Chart.js Charts */
   // Sales chart
+ 
   var salesChartCanvas = document.getElementById('revenue-chart-canvas').getContext('2d')
   // $('#revenue-chart').get(0).getContext('2d');
 
   var salesChartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: data.labels,
     datasets: [
       {
         label: 'Digital Goods',
@@ -128,18 +130,7 @@ $(function () {
         pointStrokeColor: 'rgba(60,141,188,1)',
         pointHighlightFill: '#fff',
         pointHighlightStroke: 'rgba(60,141,188,1)',
-        data: [28, 48, 40, 19, 86, 27, 90]
-      },
-      {
-        label: 'Electronics',
-        backgroundColor: 'rgba(210, 214, 222, 1)',
-        borderColor: 'rgba(210, 214, 222, 1)',
-        pointRadius: false,
-        pointColor: 'rgba(210, 214, 222, 1)',
-        pointStrokeColor: '#c1c7d1',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
+        data: data.values
       }
     ]
   }
@@ -175,15 +166,11 @@ $(function () {
   // Donut Chart
   var pieChartCanvas = $('#sales-chart-canvas').get(0).getContext('2d')
   var pieData = {
-    labels: [
-      'Instore Sales',
-      'Download Sales',
-      'Mail-Order Sales'
-    ],
+    labels:data.labels,
     datasets: [
       {
-        data: [30, 12, 20],
-        backgroundColor: ['#f56954', '#00a65a', '#f39c12']
+        data: data.values,
+        backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#0000FF', '#a52a2a', '#00ffff', '#e9967a', '#ff00ff', "#4b0082", '#808000', '#660000']
       }
     ]
   }
@@ -203,6 +190,7 @@ $(function () {
     options: pieOptions
   })
 
+}
   // Sales graph chart
   var salesGraphChartCanvas = $('#line-chart').get(0).getContext('2d')
   // $('#revenue-chart').get(0).getContext('2d');
