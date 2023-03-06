1fedd9bfc4aff467bbdd4f948ede5bb9cb33d20c
saumyanightwalkers <saumyas@knowledgesynonyms.com>
2023-03-06 15:45:22 +0530

    add table

---
+++ api/models/SQLs/trainer_events.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `ks_ad_crmdb`.`trainer_events` (`id` INT(11) NOT NULL AUTO_INCREMENT , `user_id` INT(11) NOT NULL , `event_short_desc` VARCHAR(300) NOT NULL , `registration_link` VARCHAR(300) NOT NULL , `created_at` DATETIME NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
