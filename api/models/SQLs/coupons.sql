f91b5c10d292bddd7a479a2167d497d422f6ee29
surojit19 <surojit19@gmail.com>
2023-05-19 17:30:43 +0530

    coupon code SQL

--- api/models/SQLs/coupons.sql
+++ api/models/SQLs/coupons.sql
@@ -1,16 +1,20 @@
 CREATE TABLE `trainer_coupons` (
   `id` int(11) NOT NULL,
-  `trainer_id` INT(11) NOT NULL,
+  `trainer_id` int(11) NOT NULL,
   `coupon_code` varchar(200) NOT NULL,
-  `usage_limit` int(11) NOT NULL DEFAULT 0,
-  `course_ids` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `usage_limit` int(11) DEFAULT NULL,
+  `course_ids` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `expiry_date` date DEFAULT NULL,
   `coupon_type` int(11) NOT NULL,
   `discount_value` int(11) NOT NULL,
-  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `trainer_coupons` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`id`);
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-ALTER TABLE `trainer_coupons` ADD UNIQUE(`coupon_code`);
+ALTER TABLE `trainer_coupons`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `coupon_code` (`coupon_code`);
+  
+  
+  ALTER TABLE `trainer_coupons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
